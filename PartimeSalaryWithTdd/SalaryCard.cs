48c4c8c956c18b25d101793c3ea4269e174a032f
Joey Chen <joeychen@yahoo-inc.com>
2015-05-29 12:26:06 +0800

    top-down design: depend on GetWorkingHour() function to interact with DatetimeHelper

---
+++ PartimeSalaryWithTdd/SalaryCard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PartimeSalaryWithTdd
+{
+	public class SalaryCard
+	{
+		public int HourlySalary { get; set; }
+
+		public DateTime StartTime { get; set; }
+
+		public DateTime EndTime { get; set; }
+
+		public int CalculateSalary()
+		{
+			var workingHour = this.GetWorkingHour();
+			var result = workingHour * this.HourlySalary;
+
+			return result;
+		}
+
+		private double GetWorkingHour()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
