2de93a1e1f5a1912fb6c4fe4b7587aba48b354a5
Joey Chen <joeychen@yahoo-inc.com>
2015-05-29 12:32:48 +0800

    add second test case for FirstOverTime, only add test cases without production code

--- PartimeSalaryWithTdd.Tests/SalaryCardTests.cs
+++ PartimeSalaryWithTdd.Tests/SalaryCardTests.cs
@@ -85,5 +85,33 @@ namespace PartimeSalaryWithTdd.Tests
           
         }
 
+		[TestMethod]
+		public void TestCalculateSalary_加班2小時_加班頭2小時比例為1點66_薪資應為1132()
+		{
+			//Scenario: 加班2小時：
+			//    上班8小時，時薪100乘以8，加班2小時，加班費100乘以1.66乘以2，薪資應為1132
+			//    Given 正常上班一小時薪資為 100
+			//    And 加班薪資頭2小時，時薪比例為 1.66
+			//    And 上班時間為 "2014/8/30 08:00:00"
+			//    And 下班時間為 "2014/8/30 19:00:00"
+			//    When 呼叫CalculateSalary方法
+			//    Then 薪資計算結果應為 1132
+
+			var target = new SalaryCard();
+			target.HourlySalary = 100;
+
+			//新增property
+			target.FirstOverTimeRatio = 1.66;
+			target.StartTime = new DateTime(2014, 8, 30, 8, 0, 0);
+			target.EndTime = new DateTime(2014, 8, 30, 19, 0, 0);
+
+			//act
+			var actual = target.CalculateSalary();
+
+			//assert
+			var expected = 1132;
+			Assert.AreEqual(expected, actual);
+		}
+
     }
 }
